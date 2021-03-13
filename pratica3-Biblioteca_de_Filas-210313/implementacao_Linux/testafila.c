bf68788404be784bb170ff9a1187f195b1f4d05b
Leonardo Basso Furini <leonardo.furini@hotmail.com>
2021-03-13 16:46:06 -0300

    Update testafila.c

--- pratica3-Biblioteca_de_Filas-210313/implementacao_Linux/testafila.c
+++ pratica3-Biblioteca_de_Filas-210313/implementacao_Linux/testafila.c
@@ -44,9 +44,9 @@ void print_elem (void *ptr)
    if (!elem)
       return ;
 
-   elem->prev ? printf (" %d", elem->prev->id) : printf ("*") ;
+   elem->prev ? printf ("%d", elem->prev->id) : printf ("*") ;
    printf ("<%d>", elem->id) ;
-   elem->next ? printf ("%d", elem->next->id) : printf ("*") ;
+   elem->next ? printf ("%d ", elem->next->id) : printf ("* ") ;
 }
 
 //------------------------------------------------------------------------------
@@ -330,7 +330,7 @@ int main (int argc, char **argv, char **envp)
       queue_append ((queue_t**) &fila0, (queue_t*) &item[i]) ;
 
    // imprimir a fila
-   printf ("Saida esperada: [9<0>1 0<1>2 1<2>3 2<3>4 3<4>5 4<5>6 5<6>7 6<7>8 7<8>9 8<9>0]\n") ;
+   printf ("Saida esperada: [9<0>1 0<1>2 1<2>3 2<3>4 3<4>5 4<5>6 5<6>7 6<7>8 7<8>9 8<9>0 ]\n") ;
    queue_print ("Saida gerada  ", (queue_t*) fila0, print_elem) ;
 
    printf ("Testes concluidos!!!\n") ;

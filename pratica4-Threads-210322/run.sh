8347544dc4cb9a1a1ceeda1bf4ae4b877290bf29
Leonardo Basso Furini <leonardo.furini@hotmail.com>
2021-03-23 18:51:28 -0300

    Pratica 4
    Compilação com a biblioteca pthread

--- pratica4-Threads-210322/run.sh
+++ pratica4-Threads-210322/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Executa os 3 algotimos e salva a saida em um arquivo
-gcc -Wall thread-creat.c -o thread-creat
-gcc -Wall thread-join.c -o thread-join
-gcc -Wall thread-print.c -o thread-print
+gcc -Wall thread-creat.c -o thread-creat -lpthread
+gcc -Wall thread-join.c -o thread-join -lpthread
+gcc -Wall thread-print.c -o thread-print -lpthread
 
 echo "Pratica 2 - 210308" > ResultadoSaida
 

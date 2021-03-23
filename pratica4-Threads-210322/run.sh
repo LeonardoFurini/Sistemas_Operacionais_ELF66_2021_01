1382b7fb944887676365a7402e6d1bafd8a2f4e5
Leonardo Furini <leonardo.b.furini@gmail.com>
2021-03-23 21:57:51 +0000

    correcao

--- pratica4-Threads-210322/run.sh
+++ pratica4-Threads-210322/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Executa os 3 algotimos e salva a saida em um arquivo
-gcc -Wall thread-creat.c -o thread-creat -lpthread
+gcc -Wall thread-create.c -o thread-create -lpthread
 gcc -Wall thread-join.c -o thread-join -lpthread
 gcc -Wall thread-print.c -o thread-print -lpthread
 
@@ -9,7 +9,7 @@ echo "Pratica 2 - 210308" > ResultadoSaida
 
 
 echo "---------- Exercício 1 ----------" >> ResultadoSaida
-./thread-creat >> ResultadoSaida
+./thread-create >> ResultadoSaida
 
 echo "---------- Exercício 2 ----------" >> ResultadoSaida
 ./thread-join >> ResultadoSaida
